dde0fb4a0cfc2df7d2f3f3d0a74da1ad331c0ca7
kurvin <kurvinhendricks@gmail.com>
2017-12-30 17:13:06 +0200

    initial upload, bootstrap, laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+        <title>Metro - App</title>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            
+        </div>
+    </body>
+    <footer>
+        <script type="text/javascript" src="../js/popper.min.js"></script>
+        <script type="text/javascript" src="../js/jquery.min.js"></script>
+        <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    </footer>
+</html>
