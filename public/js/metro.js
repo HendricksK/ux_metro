a63c3c066101e28a1c149b2d5be772c7bdc203a7
kurvin <kurvinhendricks@gmail.com>
2017-12-30 23:11:08 +0200

    more work on the base template, will just need to add API info to .ENV file, and connect front end to API, just working on form serialization for the API to get the formatted data

--- public/js/metro.js
+++ public/js/metro.js
@@ -1,8 +1,14 @@
 (function(){
 
-	if(document.getElementById('metro-user-input')) {
+	if(document.getElementById('metroUserInput')) {
 		postMetroInput()			
 	}
+
+	$(".dropdown-menu li").click(function(){
+  		var selText = $(this).text()
+  		console.log($(this).parents('.dropdown.btn'))
+  		$(this).parents('.dropdown').find('button').text(selText)
+	});
 
 })()
 
@@ -10,9 +16,11 @@ function postMetroInput() {
 	/*
 	need to add the rest of the XHR call here
 	*/
-
-	// var form = document.querySelector('form')
-	// var data = new FormData(form)
+	$('#metroUserInput').on('submit', function(){
+		var form = document.getElementById('metroUserInput')
+		var data = new FormData(form)
+		window.console.log(data)
+	})
 	// var req = new XMLHttpRequest()
 	// req.send(data)
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,33 +16,43 @@
                     <img width="100%" src="../assets/images/Burj_Khalifa.jpg" class="rounded" alt="...">    
                 </div>
                 <div class="col-6">
-                    @include('navigation.breadcrumb') 
-                    <form class="w-100" id="metro-user-input">
-                        <div class="form-group">
-                            <label for="transportId">Email address</label>
-                            <input type="text" class="form-control" id="transportId" aria-describedby="Transport ID" placeholder="Transport ID eg: 0925">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                        </div>
-                        <div class="form-group">
-                            <label id="transportStatus">Is your transport late?</label>
-                            <div class="radio">
-                                <label>Yes </label>
-                                <input type="radio" name="optradio">
+                    @include('navigation.breadcrumb')
+                    <div class="container w-75 justify-content-center"> 
+                        <form class="w-100" id="metroUserInput">
+                            <div class="form-group">
+                                <label for="transportId">Transport ID</label>
+                                <input type="text" class="form-control" id="transportId" name="transportId" aria-describedby="Transport ID" placeholder="Transport ID eg: 0925">
                             </div>
-                            <div class="radio">
-                                <label>No </label>
-                                <input type="radio" name="optradio" checked>
+                            <div class="form-group">
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" name="selectedStation" id="selectedStation" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Select station
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        @foreach ($stations as $station)
+                                             <li class="dropdown-item"> {{ $station }}</li>
+                                        @endforeach
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <label id="transportStatus">Would you like to add any more detail? 144 characters only.</label>
-                            <textarea class="form-control" rows="5" id="extraInfo"  maxlength="144"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
+                            <div class="form-group">
+                                <label>Is your transport late?</label>
+                                <div class="radio">
+                                    <label>Yes&nbsp;&nbsp;</label>
+                                    <input type="radio" id="ontime" name="ontime">
+                                </div>
+                                <div class="radio">
+                                    <label>No&nbsp;&nbsp;</label>
+                                    <input type="radio" id="late" name="late">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Would you like to add any more detail? 144 characters only.</label>
+                                <textarea id="transportExtra" name="transportExtra" class="form-control" rows="2" id="extraInfo"  maxlength="144"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
                 </div>
                 <div class="col-3">
                     <img width="100%" src="../assets/images/Burj_Khalifa.jpg" class="rounded" alt="...">    
@@ -51,9 +61,9 @@
         </div>
     </body>
     <footer>
-        <script type="text/javascript" src="../js/popper.min.js"></script>
-        <script type="text/javascript" src="../js/jquery.min.js"></script>
-        <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
         <script type="text/javascript" src="../js/metro.js"></script>
     </footer>
 </html>
