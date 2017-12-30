7f60549f1771b4624a574f5c33a332c6da5e195d
kurvin <kurvinhendricks@gmail.com>
2017-12-30 18:09:35 +0200

    removing bootstrap from composer autoload, added to public also working on html for home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,39 @@
         <title>Metro - App</title>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            
+        <div class="container">
+            <div class="row">
+                <form class="w-75 p-3">
+                    <div class="form-group">
+                        <label for="transportId">Email address</label>
+                        <input type="text" class="form-control" id="transportId" aria-describedby="Transport ID" placeholder="Transport ID eg: 0925">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <label id="transportStatus">Is your transport late?</label>
+                    <div class="form-group">
+                        <div class="radio">
+                            <label>Option 1</label><br>
+                            <input type="radio" name="optradio">
+                        </div>
+                        <div class="radio">
+                            <label>Option 2</label><br>
+                            <input type="radio" name="optradio">
+                        </div>
+                        <!-- <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <input type="radio" aria-label="Radio button for following text input">
+                                <label for="transportId">Email address</label>
+                                <input type="radio" aria-label="Radio button for following text input">
+                                <label for="transportId">Email address</label>
+                            </div>
+                        </div> -->
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>     
+            </div>
         </div>
     </body>
     <footer>
